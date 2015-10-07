166b99026a0b8a190e04e82902157171ba8c551d
minhnv <minhnv@aoi-sys.vn>
2015-10-07 09:39:01 +0700

    Login

--- helloworld.py
+++ helloworld.py
@@ -9,11 +9,13 @@ class MainPage (webapp2.RequestHandler):
 		user = users.get_current_user()
 
 		if user:
-			self.response.headers['Content-Type'] = 'text/html; charset=uft-8'
-			self.response.write('Hello, '+ user.nickname())
+			# self.response.headers['Content-Type'] = 'text/html; charset=uft-8'
+			# self.response.write('Hello, '+ user.nickname())
+			greeting = ('Welcome, %s (<a href="%s"> Sign Out </a>)' % (user.nickname(), users.create_logout_url('/')))
 		else:
-			self.redirect(users.create_login_url(self.request.uri))
-
+			greeting = ('<a href="%s"> Sign in or register </a>' % (users.create_login_url('/')))
+			#self.redirect(users.create_login_url(self.request.uri))
+		self.response.out.write('<html><body>%s <body></html>' % greeting)
 app = webapp2.WSGIApplication([
 	('/',MainPage)
 ], debug=True)
