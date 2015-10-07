3296cd234075898a99bd479773d5f7aabcbabfe6
minhnv <minhnv@aoi-sys.vn>
2015-10-07 09:19:19 +0700

    Using

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,19 @@
 import webapp2
+from google.appengine.api import users
+
+
 
 class MainPage (webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type']='Text/plain'
-		self.response.write ('Hello, World')
-app= webapp2.WSGIApplication([
-	('/', MainPage)
-], debug= True)
+		#Check for active for accont session
+		user = users.get_current_user()
+
+		if user:
+			self.response.headers['Content-Type'] = 'text/html; charset=uft-8'
+			self.response.write('Hello, '+ user.nickname())
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+
+app = webapp2.WSGIApplication([
+	('/',MainPage)
+], debug=True)
