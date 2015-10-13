c7556632a2f3a6f4a716afdaf7643ccda2ceb738
minhnv <minhnv@aoi-sys.vn>
2015-10-13 15:51:43 +0700

    Guestbook FormView

--- django-guestbook/guestbook/urls.py
+++ django-guestbook/guestbook/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from guestbook.views import MainView, SignView
 
 urlpatterns = patterns('',
-                       (r'^sign/$', SignView.as_view()),
-                       (r'^$', MainView.as_view()),
-                       )
+    (r'^sign/$', SignView.as_view()),
+    (r'^$', MainView.as_view()),
+)
--- django-guestbook/guestbook/views.py
+++ django-guestbook/guestbook/views.py
@@ -1,45 +1,42 @@
-import logging
+from google.appengine.api import users
 from django import forms
+from django.contrib import messages
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
-from google.appengine.api import users
 
 from guestbook.models import Greeting, guestbook_key, DEFAULT_GUESTBOOK_NAME
 
-
 class MainView(TemplateView):
-	template_name = 'guestbook/index.html'
+    template_name = 'guestbook/index.html'
 
-	def get_context_data(self, *args, **kwargs):
-		guestbook_name = self.request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-		greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(
-			-Greeting.date)
-		greetings = greetings_query.fetch(10)
-		# logging.info(greetings)
-		if users.get_current_user():
-			url = users.create_logout_url(self.request.get_full_path())
-			url_linktext = 'Logout'
-		else:
-			url = users.create_login_url(self.request.get_full_path())
-			url_linktext = 'Login'
+    def get_context_data(self, *args, **kwargs):
+        guestbook_name = self.request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
+        greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.get_full_path())
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.get_full_path())
+            url_linktext = 'Login'
 
-		template_values = {
-			'greetings': greetings,
-			'guestbook_name': guestbook_name,
-			'url': url,
-			'url_linktext': url_linktext,
-		}
-		context = super(MainView, self).get_context_data(**kwargs)
-		context['greetings'] = template_values
-		return context
+        template_values = {
+            'greetings': greetings,
+            'guestbook_name': guestbook_name,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['greetings'] = template_values
+        return context
 
 
 class SignForm(forms.Form):
-	guestbook_name = forms.CharField(
-        label='Gguestbook name',
+    guestbook_name = forms.CharField(
+        label='Guestbook name',
         max_length=50
     )
-	guestkook_mesage = forms.CharField(
+    guestkook_mesage = forms.CharField(
         widget=forms.Textarea,
         label='Guestkook mesage',
         max_length=100
@@ -49,36 +46,23 @@ class SignForm(forms.Form):
 class SignView(FormView):
     template_name = 'guestbook/sign.html'
     form_class = SignForm
-
+    success_url="/sign/"
     def get_initial(self):
         initial = super(SignView, self).get_initial()
         guestbook_name = self.request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-        # greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(
-        # -Greeting.date)
-        # greetings = greetings_query.fetch(10)
-        # logging.info(greetings)
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.get_full_path())
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.get_full_path())
-            url_linktext = 'Login'
-
-        template_data = {
-            # 'greetings': greetings,
-            'guestbook_name': guestbook_name,
-        }
-        initial['data_login']=template_data
-        logging.info(initial)
+        initial['guestbook_name']=guestbook_name
         return initial
 
-    def form_invalid(self, form):
-        guestbook_name = self.request.POST.get('guestbook_name')
+    def form_valid(self, form):
+        guestbook_name = form.cleaned_data['guestbook_name']
         greeting = Greeting(parent=guestbook_key(guestbook_name))
-
         if users.get_current_user():
             greeting.author = users.get_current_user()
-
-        greeting.content = self.request.POST.get('content')
+        greeting.content = form.cleaned_data['guestkook_mesage']
         greeting.put()
-        return HttpResponseRedirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
+        messages.warning(self.request, '%s created successfully.' % guestbook_name)
+        return super(SignView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        messages.warning(self.request, 'Please input field!')
+        return super(SignView, self).form_invalid(form)
