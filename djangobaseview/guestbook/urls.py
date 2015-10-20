1e5a0a96b3880de61a7f595697c171a2aaa3d050
minhnv <minhnv@aoi-sys.vn>
2015-10-20 13:38:32 +0700

    fix send m

--- djangobaseview/guestbook/urls.py
+++ djangobaseview/guestbook/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns,url
-from guestbook.views import IndexView, SignView, send_mail
+from guestbook.views import IndexView, SignView, SendmailView
 
 urlpatterns = patterns('',
 	url(r'^$', IndexView.as_view()),
 	url(r'^sign/$', SignView.as_view()),
-	url(r'^sendmail/$', send_mail),
+	url(r'^sendmail/$', SendmailView.as_view()),
 )
--- djangobaseview/guestbook/views.py
+++ djangobaseview/guestbook/views.py
@@ -70,7 +70,7 @@ class SignView(FormView):
 		messages.success(self.request, '%s created successfully.' % guestbook_name)
 		user = users.get_current_user()
 		if user:
-			taskqueue.add(url = '/sign',params = {'email': user.email()},method = 'GET')
+			taskqueue.add(url = '/sendmail/',params = {'email': user.email()},method = 'GET')
 		return super(SignView, self).form_valid(form)
 
 	def form_invalid(self, form):
@@ -78,48 +78,19 @@ class SignView(FormView):
 		return super(SignView, self).form_invalid(form)
 
 
-def send_mail(request):
-	logging.info('begin')
-	if request.method == 'GET':
-		logging.info('begin 12')
-		user = users.get_current_user()
-		if user:
+class SendmailView(TemplateView):
+	def get(self, request, *args, **kwargs):
+		email = request.GET.get('email')
+		if email:
 			message= mail.EmailMessage()
-			message.sender=user.email()
-			message.to = user.email()
+			message.sender = email
+			message.to = email
 			message.subject = 'Test'
 			message.body= """
-							Dear Albert:
-							Your example.com account has been approved.  You can now visit
-							http://www.example.com/ and sign in using your Google Account to
-							access new features.
-							Please let us know if you have any questions.
-							The example.com Team
+							Wellcom
+							You creat Greeting
 							"""
 			message.send()
 			logging.info(message)
-			return HttpResponseRedirect('/')
-	return HttpResponseRedirect('/')
-
-# class SendmailView(TemplateView):
-# 	logging.info('begin')
-# 	# def get(self, request, *args, **kwargs):
-# 		logging.info('begin 1')
-# 		email = request.GET.get('email')
-# 		if email:
-# 			# message= mail.EmailMessage()
-# 			# message.sender = email
-# 			# message.to = email
-# 			# message.subject = 'Test'
-# 			# message.body= """
-# 			# 				Dear Albert:
-# 			# 				Your example.com account has been approved.  You can now visit
-# 			# 				http://www.example.com/ and sign in using your Google Account to
-# 			# 				access new features.
-# 			# 				Please let us know if you have any questions.
-# 			# 				The example.com Team
-# 			# 				"""
-# 			# message.send()
-# 			logging.info(message)
-# 			return HttpResponseRedirect('/sign')
-# 		return HttpResponseRedirect('/')
+			return HttpResponseRedirect('/sign')
+		return HttpResponseRedirect('/')
