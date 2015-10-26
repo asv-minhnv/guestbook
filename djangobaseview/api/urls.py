ca5daeefa3a7fb00e7dadd958ea591b0d857f04d
minhnv <minhnv@aoi-sys.vn>
2015-10-26 15:10:36 +0700

    fix api

--- djangobaseview/api/urls.py
+++ djangobaseview/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns,url
 # from api.views import IndexView
-from api.apis import GetListView,ResourceSinge
+from api.restful import GetListView,ResourceSinge
+
 urlpatterns = patterns('',
 	url(r'^guestbook/(?P<guestbook_name>.+)/greeting/(?P<id>\d+)$', ResourceSinge.as_view()),
 	url(r'^guestbook/(?P<guestbook_name>).+/greeting/$', GetListView.as_view()),
